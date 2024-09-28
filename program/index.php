025800ea1b82bb7302120569a152a65995f091b5
DrSeres <112695233+DrSeres@users.noreply.github.com>
2024-09-28 11:22:08 +0200

    Főoldal - Legjobbra értékelt tanárok lekérdezése

--- program/index.php
+++ program/index.php
@@ -2,7 +2,9 @@
 
 include_once("database.php");
 
-$sql = "SELECT tanarok.vezeteknev, tanarok.keresztnev, tanarok.id, tantargyak.nev FROM `tantargykapcsolotabla` inner join tanarok on tantargykapcsolotabla.tanar_id = tanarok.id inner JOIN tantargyak on tantargyak.id = tantargykapcsolotabla.tantargy_id";
+// Összes tanár lekérdezése
+
+$sql = "SELECT tanarok.vezeteknev as 'vezeteknev', tanarok.keresztnev as 'keresztnev', tanarok.id as 'id', tantargyak.nev as 'nev' FROM `tantargykapcsolotabla` inner join tanarok on tantargykapcsolotabla.tanar_id = tanarok.id inner JOIN tantargyak on tantargyak.id = tantargykapcsolotabla.tantargy_id";
 $query = mysqli_query($conn, $sql);
 
 
@@ -14,14 +16,30 @@ if(mysqli_num_rows($query) > 0){
         $table .=  
         "<div class='box'>
             <img src={$picture} alt='tanar kep' title='tanar kep' width=200px height=200px />
-            <h3>{$row["nev"]}</h3>
             <p>{$row["vezeteknev"]} {$row["keresztnev"]}</p>
+            <h3>{$row["nev"]}</h3>
             <a href='informacio.php?id={$row["id"]}'><button type='button'>Információk</button></a>
         </div>";
     }
     $table .= "</div>";
 }
 
+// Legjobbra értékelt tanárok lekérdezése
+
+$sqlTop = "SELECT tanarok.vezeteknev AS 'vezeteknev', tanarok.keresztnev AS 'keresztnev', round(avg(ertekelesek.ertekeles), 1) AS 'atlag' FROM `tanarok` inner JOIN ertekelesek on tanarok.id = ertekelesek.tanar_id GROUP BY tanarok.vezeteknev, tanarok.keresztnev ORDER BY atlag DESC limit 6";
+$queryTop = mysqli_query($conn, $sqlTop);
+
+$topTable = "<div class='toplistacontainer'>";
+
+if(mysqli_num_rows($queryTop) > 0){
+    while($top = mysqli_fetch_assoc($queryTop)){
+        $topTable .=  
+        "<div class='topBox'>
+            <p>{$top["vezeteknev"]} {$top["keresztnev"]} {$top["atlag"]}</p>
+        </div>";
+    }
+    $topTable .= "</div>";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,8 +47,12 @@ if(mysqli_num_rows($query) > 0){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Főoldal</title>
+    
 </head>
 <body>
+    <h1>Legjobbra értékelt tanárok</h1>
+    <?php echo $topTable ?>
+
     <?php echo $table  ?>
 </body>
 </html>
