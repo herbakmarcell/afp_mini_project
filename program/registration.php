75384b515870cbd357d3d2cce3a1f82d136b7216
Vastag Demeter <demetervastag05@gmail.com>
2024-09-30 22:56:35 +0200

    Registration.php törölve
    A registration.html-be másoltam át a scriptet, majd töröltem a registration.php-t.

--- program/registration.php
+++ program/registration.php
@@ -3,99 +3,102 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Regisztráció</title>
 </head>
-    <?php 
-    $hibak = [];
-    require_once './database.php';
-    require_once './functions.php';
-        if($_SERVER['REQUEST_METHOD'] === 'POST')
+<?php 
+$hibak = [];
+require_once './database.php';
+require_once './functions.php';
+    if($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+        $jo_adatok = 0;
+        $felhasznalonev_helyes = check_data("fnev|nem_ures");
+        if($felhasznalonev_helyes)
         {
-            $jo_adatok = 0;
-            $felhasznalonev_helyes = check_data("fnev|nem_ures");
-            if($felhasznalonev_helyes)
-            {
-                $felhasznalonev = tisztit($_POST['fnev']);
-                $jo_adatok++;
-            }
-            else 
-                $hibak[] = 'A felhasználónév nem megfelelő!';
-
-
-            $email_helyes = check_data('email|nem_ures');
-            if($email_helyes)
-            {
-                $email  = tisztit($_POST['email']);
-                $jo_adatok++;
-            }
-            else
-                $hibak[] = 'Az email cím nem megfelelő!';
-
-            $jelszo_helyes = check_data('jelszo|nem_ures,jelszo_megfelel');
-            if($jelszo_helyes)
-            {
-                $jelszo = tisztit($_POST['jelszo']);
-                $jo_adatok++;
-            }
-            else
-                $hibak[] = 'A jelszó nem megfelelő!';
-
-            $jelszo_ujra_helyes = check_data('jelszoujra|nem_ures,jelszo_megfelel');
-            if($jelszo_ujra_helyes)
-            {
-                $jelszo_ujra = tisztit($_POST['jelszoujra']);
-                if($jelszo_ujra != $jelszo)
-                    $hibak[] = 'A jelszavak nem egyeznek!';
-                else 
-                    $jo_adatok++;
-            }
-            else
-                $hibak[] = 'A "jelszó megerősítése" nem megfelelő!';
-
-
-            if($jo_adatok == 4)
-            {
-
-                //Utolsó id lekérdezése
-                $lekerdezes = "select max(id) from felhasznalok;";
-                if($eredmeny = $conn -> query($lekerdezes))
-                {
-                    $id =  $eredmeny -> fetch_column();
-                    $id++;
-                
-                 
-                }
-
-
-
-
-                $lekerdezes = "select * from felhasznalok where email = '".$email."';";
-                if($eredmeny = $conn -> query($lekerdezes))
-                {
-                    if($eredmeny->num_rows > 0)
-                    {
-                        $hibak[] = 'Már szerepel felhasználó a megadott email címmel!';
-                    }
-                    else 
-                    {
-                        $lekerdezes = "insert into felhasznalok (id,nev,email,jelszo) values (".$id.",'".$felhasznalonev. "','". $email . "','" .$jelszo ."');";
-                        if($eredmeny = $conn -> query($lekerdezes))
-                        {
-                            echo "A regisztráció sikeres!";
-                        }
-                    }
-                }
-            }
-
-            
-
+            $felhasznalonev = tisztit($_POST['fnev']);
+            $jo_adatok++;
         }
-    
-    
-    
-    
-    ?>
+        else 
+            $hibak[] = 'A felhasználónév nem megfelelő!';
+
+
+        $email_helyes = check_data('email|nem_ures');
+        if($email_helyes)
+        {
+            $email  = tisztit($_POST['email']);
+            $jo_adatok++;
+        }
+        else
+            $hibak[] = 'Az email cím nem megfelelő!';
+
+        $jelszo_helyes = check_data('jelszo|nem_ures,jelszo_megfelel');
+        if($jelszo_helyes)
+        {
+            $jelszo = tisztit($_POST['jelszo']);
+            $jo_adatok++;
+        }
+        else
+            $hibak[] = 'A jelszó nem megfelelő!';
+
+        $jelszo_ujra_helyes = check_data('jelszoujra|nem_ures,jelszo_megfelel');
+        if($jelszo_ujra_helyes)
+        {
+            $jelszo_ujra = tisztit($_POST['jelszoujra']);
+            if($jelszo_ujra != $jelszo)
+                $hibak[] = 'A jelszavak nem egyeznek!';
+            else 
+                $jo_adatok++;
+        }
+        else
+            $hibak[] = 'A "jelszó megerősítése" nem megfelelő!';
+
+
+        if($jo_adatok == 4)
+        {
+
+            //Utolsó id lekérdezése
+            $lekerdezes = "select max(id) from felhasznalok;";
+            if($eredmeny = $conn -> query($lekerdezes))
+            {
+                $id =  $eredmeny -> fetch_column();
+                $id++;
+            
+             
+            }
+
+
+
+
+            $lekerdezes = "select * from felhasznalok where email = '".$email."';";
+            if($eredmeny = $conn -> query($lekerdezes))
+            {
+                if($eredmeny->num_rows > 0)
+                {
+                    $hibak[] = 'Már szerepel felhasználó a megadott email címmel!';
+                }
+                else 
+                {
+                    $lekerdezes = "insert into felhasznalok (id,nev,email,jelszo) values (".$id.",'".$felhasznalonev. "','". $email . "','" .$jelszo ."');";
+                    if($eredmeny = $conn -> query($lekerdezes))
+                    {
+                        echo "A regisztráció sikeres!";
+                    }
+                }
+            }
+        }
+
+        
+
+    }
+
+
+
+
+?>
+
 
 <body>
     <nav>
@@ -107,8 +110,15 @@
         </a>
         <div class="linkek">
             <ul>
-                <li><a href="login.php">Bejelentkezés</a></li>
-                <li><a href="registration.html" class="active">Regisztráció</a></li>
+                <li>
+                    <a href="index.php"><span id="hazIkon" class="material-symbols-outlined">home</span>Főoldal</a>
+                </li>
+                <li>
+                    <a href="login.php"><span id="loginIkon" class="material-symbols-outlined">login</span>Bejelentkezés</a>
+                </li>
+                <li>
+                    <a href="registration.html" class="active"><span id="regIkon" class="material-symbols-outlined">arrow_upward</span>Regisztráció</a>
+                </li>
             </ul>
         </div>
     </nav>
@@ -129,9 +139,6 @@
                     <input type="submit" value="Regisztráció" id="belepes" name="belepes">
                     <p>Már van fiókja? <a href="login.php">Jelentkezzen be!</a></p>
                 </form>
-                <?php foreach ($hibak as $hiba): ?>
-                    <p><?= $hiba ?></p>
-                <?php endforeach; ?>
             </div>
         </div>
     </div>
