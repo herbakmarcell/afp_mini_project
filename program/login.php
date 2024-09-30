ee06fc2edeb2d82c1a468c4d4b5d41a28b6fd7ac
Vastag Demeter <demetervastag05@gmail.com>
2024-09-30 22:51:41 +0200

    'die' sor törölve

--- program/login.php
+++ program/login.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Bejelentkezés</title>
 </head>
@@ -53,7 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
     $conn->close();
-    die();
+   
 } else {
     http_response_code(405);
 }
@@ -71,15 +69,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </a>
         <div class="linkek">
             <ul>
-                <li>
-                    <a href="index.php"><span id="hazIkon" class="material-symbols-outlined">home</span>Főoldal</a>
-                </li>
-                <li>
-                    <a href="login.php" class="active"><span id="loginIkon" class="material-symbols-outlined">login</span>Bejelentkezés</a>
-                </li>
-                <li>
-                    <a href="registration.html"><span id="regIkon" class="material-symbols-outlined">arrow_upward</span>Regisztráció</a>
-                </li>
+                <li><a href="login.php" class="active">Bejelentkezés</a></li>
+                <li><a href="registration.php">Regisztráció</a></li>
             </ul>
         </div>
     </nav>
