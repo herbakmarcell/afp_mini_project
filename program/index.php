41b9a08bf5eb081501105830826c32a42656c36b
SzaboRichard01 <ricsi5822@gmail.com>
2024-10-02 09:27:05 +0200

    Favicon hozzáadás

--- program/index.php
+++ program/index.php
@@ -128,6 +128,7 @@ $topTable .= "</div>";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Főoldal</title>
+    <link rel="icon" type="image/x-icon" href="projectImg/home-favicon.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
--- program/informacio.php
+++ program/informacio.php
@@ -55,6 +55,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="projectImg/info-favicon.png">
     <title>Tanár oldal</title>
 </head>
 <body>
