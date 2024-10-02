9315ea46987237b04b4a25ba713905ad15a69660
BMVHLESzklRichard <112695477+BMVHLESzklRichard@users.noreply.github.com>
2024-10-02 19:54:07 +0200

    InformációOldal frontend elkészítve

--- program/informacio.php
+++ program/informacio.php
@@ -5,8 +5,7 @@ session_start();
 $userId = $_SESSION['user_id'];
 $teacherId = $_GET['id'];
 
-$sqlTeacher = "SELECT * FROM tanarok INNER JOIN tantargykapcsolotabla on 
-tantargykapcsolotabla.tantargy_id = tanarok.id INNER JOIN tantargyak on tantargykapcsolotabla.tantargy_id = tantargyak.id GROUP BY tanarok.vezeteknev, tanarok.keresztnev, nev HAVING tanarok.id = $teacherId";
+$sqlTeacher = "SELECT * FROM tanarok INNER JOIN tantargykapcsolotabla on tantargykapcsolotabla.tanar_id = tanarok.id INNER JOIN tantargyak on tantargykapcsolotabla.tantargy_id = tantargyak.id GROUP BY tanarok.vezeteknev, tanarok.keresztnev, nev HAVING tanarok.id = $teacherId";
 $queryTeacher = mysqli_query($conn, $sqlTeacher);
 
 $table = "<div>";
@@ -14,8 +13,9 @@ $starBtn = "";
 if (mysqli_num_rows($queryTeacher) > 0) {
     while ($row = mysqli_fetch_assoc($queryTeacher)) {
         $table .= "
-          <h2>{$row["vezeteknev"]} {$row["keresztnev"]}</h2>
-          <p>{$row["nev"]}</p>
+          <h3>Név: {$row["vezeteknev"]} {$row["keresztnev"]}</h3>
+          <p>Tantárgy: {$row["nev"]}</p>
+          <p>Átlag értékelés: {$row["atlag"]}</p>
         ";
     }
 }
@@ -66,23 +66,79 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="projectImg/info-favicon.png">
     <title>Tanár oldal</title>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="fooldal.css">
+    <link rel="stylesheet" href="informacio.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 <body>
-    <form action="" method="post">
-        <?php echo $table; ?>
+    <nav>
+        <p class="title"><a href="index.php">Tanár értékelő</a></p>
+        <a href="#" class="hmenu">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </a>
+        <div class="linkek">
+            <ul>
+                <li>
+                    <a href="index.php"><span class="material-symbols-outlined">
+                            arrow_back
+                        </span>Vissza</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <main>
+        <div class="tanarInfoDiv">
+            <div class="tanarPictureDiv\">
+                <img src=./projectImg/manFace.png alt='tanar kep' title='tanar kep' class="tanarPicture" />
+            </div>
+            <div class="tanarInfoForm">
+                <?php echo $table; ?>
+                <form action="" method="post">
 
-        <?php
 
-
-
-        ?>
-        <button type="submit" id="1" name="1"> <img src="projectImg/ratingStar.png" alt=""> </button>
-        <button type="submit" id="2" name="2"> <img src="projectImg/ratingStar.png" alt=""> </button>
-        <button type="submit" id="3" name="3"> <img src="projectImg/ratingStar.png" alt=""> </button>
-        <button type="submit" id="4" name="4"> <img src="projectImg/ratingStar.png" alt=""> </button>
-        <button type="submit" id="5" name="5"> <img src="projectImg/ratingStar.png" alt=""> </button>
-    </form>
+                    <button type="submit" id="1" name="1"> <img src="projectImg/ratingStar.png" alt=""> </button>
+                    <button type="submit" id="2" name="2"> <img src="projectImg/ratingStar.png" alt=""> </button>
+                    <button type="submit" id="3" name="3"> <img src="projectImg/ratingStar.png" alt=""> </button>
+                    <button type="submit" id="4" name="4"> <img src="projectImg/ratingStar.png" alt=""> </button>
+                    <button type="submit" id="5" name="5"> <img src="projectImg/ratingStar.png" alt=""> </button>
+                </form>
+            </div>
+        </div>
+        <div class="tanarErtekeloDiv">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi itaque harum quidem non illo, doloremque
+            amet doloribus qui cupiditate nobis esse maxime labore eius fugiat sequi veniam quo corporis. Tenetur.
+            Culpa quia cum quis, perferendis molestiae placeat repellat nobis. Cupiditate, eveniet vero! Magnam nesciunt
+            aspernatur reprehenderit nostrum praesentium laboriosam dolorum, molestias, officiis magni facere ullam
+            veniam! Eaque minus maiores hic?
+            Dolorem omnis asperiores, hic nihil quibusdam quasi natus molestiae veniam saepe illo animi aliquid ratione
+            ea tempore pariatur vitae blanditiis reprehenderit ad architecto fuga. Praesentium dicta eveniet pariatur
+            consequuntur a.
+        </div>
+    </main>
+    <footer>
+        <ul>
+            <li><a href="login.php">Bejelentkezés</a></li>
+            <li><a href="registration.php">Regisztráció</a></li>
+            <li><a href="#">Főoldal</a></li>
+        </ul>
+        <div class="socialOldalak">
+            <h2>Kövess minket:</h2>
+            <ul>
+                <li><a href="#" class="fa fa-facebook"></a><a href="">Facebook</a></li>
+                <li><a href="#" class="fa fa-linkedin"></a><a href="">Linkedin</a></li>
+                <li><a href="https://github.com/herbakmarcell/afp_mini_project.git" target="_blank"
+                        class="fa fa-github"></a><a href="https://github.com/herbakmarcell/afp_mini_project.git"
+                        target="_blank">Github</a></li>
+            </ul>
+        </div>
+    </footer>
 </body>
 
 </html>
