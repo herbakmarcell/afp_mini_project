13c426980ad3d87c508654a7d93a3bca7dd7aca8
SzaboRichard01 <ricsi5822@gmail.com>
2024-10-06 15:42:32 +0200

    sql fájl javítás

--- tanarertekelo (2).sql
+++ tanarertekelo (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Okt 06. 11:23
+-- Létrehozás ideje: 2024. Okt 06. 09:12
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -24,6 +24,77 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tábla szerkezet ehhez a táblához `ertekelesek`
+--
+
+CREATE TABLE `ertekelesek` (
+  `id` int(11) NOT NULL,
+  `felhasznalo_id` int(11) NOT NULL,
+  `tanar_id` int(11) NOT NULL,
+  `ertekeles` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `ertekelesek`
+--
+
+INSERT INTO `ertekelesek` (`id`, `felhasznalo_id`, `tanar_id`, `ertekeles`) VALUES
+(4, 3, 2, 3),
+(5, 3, 1, 5),
+(6, 2, 2, 4),
+(7, 2, 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
+--
+
+CREATE TABLE `felhasznalok` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `jelszo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `felhasznalok`
+--
+
+INSERT INTO `felhasznalok` (`id`, `nev`, `email`, `jelszo`) VALUES
+(2, 'pisti', 'pisti@gmail.com', 'e10adc3949ba59abbe56e057f20f883e'),
+(3, 'sanyi213', 'sanyi@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `kommentek`
+--
+
+CREATE TABLE `kommentek` (
+  `id` int(11) NOT NULL,
+  `felhasznalo_id` int(11) NOT NULL,
+  `tanar_id` int(11) NOT NULL,
+  `komment` text NOT NULL,
+  `ido` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `kommentek`
+--
+
+INSERT INTO `kommentek` (`id`, `felhasznalo_id`, `tanar_id`, `komment`, `ido`) VALUES
+(66, 2, 1, 'Nagyon jól magyaráz', '2024-10-05 00:00:00'),
+(70, 2, 1, 'Nagyon jól magyaráz', '2024-10-05 00:00:00'),
+(71, 2, 1, 'Nagyon jól magyaráz', '2024-10-05 12:43:28'),
+(72, 2, 2, 'Nagyon jól magyaráz', '2024-10-05 12:43:50'),
+(73, 2, 1, 'Nagyon jól magyaráz', '2024-10-05 12:44:57'),
+(74, 3, 2, 'Nagyon jól magyaráz', '2024-10-05 01:11:03'),
+(76, 3, 1, 'Nagyon jól magyaráz', '2024-10-05 01:11:55');
+
+-- --------------------------------------------------------
+
+--
 -- Tábla szerkezet ehhez a táblához `tanarok`
 --
 
@@ -40,13 +111,72 @@ CREATE TABLE `tanarok` (
 
 INSERT INTO `tanarok` (`id`, `vezeteknev`, `keresztnev`, `atlag`) VALUES
 (1, 'Nagy', 'Károly', 4.5),
-(2, 'Kovásznai', 'Gergely', 3.5),
-(8, 'Sebestyén', 'Péter', 0),
-(9, 'Troll', 'Ede', 0);
+(2, 'Kovásznai', 'Gergely', 3.5);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `tantargyak`
+--
+
+CREATE TABLE `tantargyak` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `tantargyak`
+--
+
+INSERT INTO `tantargyak` (`id`, `nev`) VALUES
+(1, 'Kalkulus'),
+(2, 'Programozás');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `tantargykapcsolotabla`
+--
+
+CREATE TABLE `tantargykapcsolotabla` (
+  `id` int(11) NOT NULL,
+  `tanar_id` int(11) NOT NULL,
+  `tantargy_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `tantargykapcsolotabla`
+--
+
+INSERT INTO `tantargykapcsolotabla` (`id`, `tanar_id`, `tantargy_id`) VALUES
+(1, 1, 1),
+(2, 2, 2);
 
 --
 -- Indexek a kiírt táblákhoz
 --
+
+--
+-- A tábla indexei `ertekelesek`
+--
+ALTER TABLE `ertekelesek`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `felhasznalo_id` (`felhasznalo_id`,`tanar_id`),
+  ADD KEY `tanar_id` (`tanar_id`);
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `kommentek`
+--
+ALTER TABLE `kommentek`
+  ADD PRIMARY KEY (`id`) USING BTREE,
+  ADD KEY `felhasznalo_id` (`felhasznalo_id`,`tanar_id`) USING BTREE,
+  ADD KEY `tanar_id` (`tanar_id`) USING BTREE;
 
 --
 -- A tábla indexei `tanarok`
@@ -55,14 +185,83 @@ ALTER TABLE `tanarok`
   ADD PRIMARY KEY (`id`);
 
 --
+-- A tábla indexei `tantargyak`
+--
+ALTER TABLE `tantargyak`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `tantargykapcsolotabla`
+--
+ALTER TABLE `tantargykapcsolotabla`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tanar_id` (`tanar_id`,`tantargy_id`),
+  ADD KEY `tantargy_id` (`tantargy_id`);
+
+--
 -- A kiírt táblák AUTO_INCREMENT értéke
 --
+
+--
+-- AUTO_INCREMENT a táblához `ertekelesek`
+--
+ALTER TABLE `ertekelesek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT a táblához `kommentek`
+--
+ALTER TABLE `kommentek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT a táblához `tanarok`
 --
 ALTER TABLE `tanarok`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT a táblához `tantargyak`
+--
+ALTER TABLE `tantargyak`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT a táblához `tantargykapcsolotabla`
+--
+ALTER TABLE `tantargykapcsolotabla`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `ertekelesek`
+--
+ALTER TABLE `ertekelesek`
+  ADD CONSTRAINT `ertekelesek_ibfk_1` FOREIGN KEY (`tanar_id`) REFERENCES `tanarok` (`id`),
+  ADD CONSTRAINT `ertekelesek_ibfk_2` FOREIGN KEY (`felhasznalo_id`) REFERENCES `felhasznalok` (`id`);
+
+--
+-- Megkötések a táblához `kommentek`
+--
+ALTER TABLE `kommentek`
+  ADD CONSTRAINT `kommentek_ibfk_1` FOREIGN KEY (`felhasznalo_id`) REFERENCES `felhasznalok` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `kommentek_ibfk_2` FOREIGN KEY (`tanar_id`) REFERENCES `tanarok` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Megkötések a táblához `tantargykapcsolotabla`
+--
+ALTER TABLE `tantargykapcsolotabla`
+  ADD CONSTRAINT `tantargykapcsolotabla_ibfk_1` FOREIGN KEY (`tanar_id`) REFERENCES `tanarok` (`id`),
+  ADD CONSTRAINT `tantargykapcsolotabla_ibfk_2` FOREIGN KEY (`tantargy_id`) REFERENCES `tantargyak` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
